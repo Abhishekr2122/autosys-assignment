19fa43f4832f6be0ab80719f258d6f7b92448223
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-02-26 19:44:22 +0530

    Working on the cards.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,40 @@
 import "./dashboard.css";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
 
 export default function Dashboard() {
-  return <div className="dashboard"></div>;
+  return (
+    <div className="dashboard">
+      <div className="cards">
+        <div className="card">
+          <div>
+            <FaHtml5 />
+            <span>Html</span>
+          </div>
+          <div>
+            <p>{`HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It defines the content and structure of web content. It is often assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.
+
+Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for its appearance.
+
+HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects such as interactive forms may be embedded into the rendered page. HTML provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes, and other items. HTML elements are delineated by tags, written using angle brackets. Tags such as <img> and <input> directly introduce content into the page. Other tags such as <p> and </p> surround and provide information about document text and may include sub-element tags. Browsers do not display the HTML tags but use them to interpret the content of the page.`}</p>
+          </div>
+        </div>
+        <div className="card">
+          <div>
+            <div>
+              <FaCss3Alt />
+              <span>Css</span>
+            </div>
+            <div>
+              <p>
+                {`Cascading Style Sheets (CSS) is a style sheet language used for specifying the presentation and styling of a document written in a markup language such as HTML or XML (including XML dialects such as SVG, MathML or XHTML).CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.
+
+CSS is designed to enable the separation of content and presentation, including layout, colors, and fonts.This separation can improve content accessibility;[further explanation needed] provide more flexibility and control in the specification of presentation characteristics; enable multiple web pages to share formatting by specifying the relevant CSS in a separate .css file, which reduces complexity and repetition in the structural content; and enable the .css file to be cached to improve the page load speed between the pages that share the file and its formatting.`}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
